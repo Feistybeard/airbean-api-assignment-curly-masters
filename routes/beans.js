12a87bcb5569b2c14d52b9c07b313c8f6311fd17
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-04-03 20:53:36 +0700

    Added beans/order

--- routes/beans.js
+++ routes/beans.js
@@ -1,7 +1,37 @@
 const { Router } = require("express");
 const router = Router();
 const uuid = require("uuid-random");
+const Datastore = require("nedb");
 
-console.log("beans.js");
+const db = new Datastore({ filename: "orders.db", autoload: true });
+
+router.post("/order", (req, res) => {
+  const { name, price } = req.body;
+  const order = {
+    id: uuid(),
+    name,
+    price,
+    date: new Date(),
+  };
+  db.insert(order, (err, newOrder) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("An error occurred");
+    } else {
+      res.send(`Order ${newOrder.id} created`);
+    }
+  });
+});
+
+router.get("/orders", (req, res) => {
+  db.find({}, (err, orders) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("An error occurred");
+    } else {
+      res.send(orders);
+    }
+  });
+});
 
 module.exports = router;

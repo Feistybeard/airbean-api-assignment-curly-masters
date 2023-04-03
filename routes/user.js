85b8d87925036e34afc7a78310b1208e7f963513
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-04-03 18:28:52 +0700

    user/login

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,36 @@
 const { Router } = require("express");
 const router = Router();
 const uuid = require("uuid-random");
+const Datastore = require("nedb");
 
-console.log("user.js");
+const db = new Datastore({ filename: "users.db", autoload: true });
+
+const user = {
+  username: "oscarmat",
+  password: "password123",
+};
+
+db.insert(user, (err, newDoc) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Inserted user:", newDoc);
+  }
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  db.find({ username, password }, (err, users) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("An error occurred");
+    } else if (users.length === 0) {
+      res.status(401).send("Invalid username or password");
+    } else {
+      const user = users[0];
+      res.send(`Welcome ${user.username}`);
+    }
+  });
+});
 
 module.exports = router;

65cd8e8e70d918c81301899f20b5d358683d19ed
Marvan Al Kufai <alkufai@gmail.com>
2023-04-03 17:49:07 +0200

    added beans/order/status

--- routes/beans.js
+++ routes/beans.js
@@ -1,7 +1,28 @@
 const { Router } = require("express");
 const router = Router();
 const uuid = require("uuid-random");
+const Datastore = require("nedb-promises");
 
-console.log("beans.js");
+const ordersDb = Datastore.create({ filename: "beans.db", autoload: true });
+
+// const order = {
+//   orderId: "123",
+//   eta: 10,
+// };
+
+// ordersDb.insert(order);
+
+router.get("/order/status/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const getOrder = await ordersDb.findOne({ orderId: id });
+  if (getOrder) {
+    res.json({ eta: getOrder.eta });
+  } else {
+    res
+      .status(200)
+      .json({ success: false, message: "Ingen beställning funnen" });
+  }
+});
 
 module.exports = router;
